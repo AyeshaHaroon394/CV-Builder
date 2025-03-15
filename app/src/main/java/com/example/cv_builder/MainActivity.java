97e8ddbb24358a2856b4b2d94060774e67483796
AyeshaHaroon394 <ayeshaharoon394@gmail.com>
2025-03-15 19:12:10 +0500

    Home Screen Added

--- app/src/main/java/com/example/cv_builder/MainActivity.java
+++ app/src/main/java/com/example/cv_builder/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.cv_builder;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,46 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnProfilePicture,
+            btnPersonalDetails,
+            btnSummary,
+            btnEducation,
+            btnExperience,
+            btnCertifications,
+            btnReferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        init();
     }
+
+    private void init() {
+        //Initializing
+        btnProfilePicture = findViewById(R.id.btnProfilePicture);
+        btnPersonalDetails = findViewById(R.id.btnPersonalDetails);
+        btnSummary = findViewById(R.id.btnSummary);
+        btnEducation = findViewById(R.id.btnEducation);
+        btnExperience = findViewById(R.id.btnExperience);
+        btnCertifications = findViewById(R.id.btnCertifications);
+        btnReferences = findViewById(R.id.btnReferences);
+
+        //Setting click listeners
+        btnProfilePicture.setOnClickListener(v -> navigateTo("ProfilePictureScreen"));
+        btnPersonalDetails.setOnClickListener(v -> navigateTo("PersonalDetailsScreen"));
+        btnSummary.setOnClickListener(v -> navigateTo("SummaryScreen"));
+        btnEducation.setOnClickListener(v -> navigateTo("EducationScreen"));
+        btnExperience.setOnClickListener(v -> navigateTo("ExperienceScreen"));
+        btnCertifications.setOnClickListener(v -> navigateTo("CertificationsScreen"));
+        btnReferences.setOnClickListener(v -> navigateTo("ReferencesScreen"));
+    }
+
+    private void navigateTo(String className) {
+        Intent intent = new Intent();
+        intent.setClassName(getPackageName(), getPackageName() + "." + className);
+        startActivity(intent);
+    }
+
 }
